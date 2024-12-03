7f03fdc2b617372b8c04e786afa6747315c68faa
Yulia Pylypets <yulia250603@gmail.com>
2024-12-03 15:51:43 +0200

    update admin panel to support assigning redactors to newspapers

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,25 +1,26 @@
 from django.contrib import admin
-from catalog.models import Topic, Newspaper, Redactor
+from .models import Topic, Newspaper, Redactor
+
 
 # Реєстрація моделі Topic
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ("name",)  # Відображення назв тем
-    search_fields = ("name",)  # Пошук за назвою теми
+    list_display = ("name",)
+    search_fields = ("name",)
 
 
 # Реєстрація моделі Redactor
 @admin.register(Redactor)
 class RedactorAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "years_of_experience")
-    search_fields = ("username", "email")  # Пошук за ім'ям користувача та поштою
-    list_filter = ("years_of_experience",)  # Фільтр за досвідом
+    search_fields = ("username", "email")
+    list_filter = ("years_of_experience",)
 
 
 # Реєстрація моделі Newspaper
 @admin.register(Newspaper)
 class NewspaperAdmin(admin.ModelAdmin):
     list_display = ("title", "published_date")
-    search_fields = ("title", "content")  # Пошук за назвою та контентом
-    list_filter = ("published_date",)  # Фільтр за датою випуску
-    filter_horizontal = ("topics", "redactors")  # Зручне додавання тем та редакторів
+    search_fields = ("title", "content")
+    list_filter = ("published_date",)
+    filter_horizontal = ("topics", "redactors")  # можливість вибору редакторів
